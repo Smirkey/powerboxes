143cbb450191b5860b83d62efdf7fb880992fdbb
Guillaume Bertrand <guillaume.bertrand59@gmail.com>
2023-11-07 22:52:31 +0100

    progress

---
+++ python/powerboxes/__init__.py
@@ -0,0 +1,3 @@
+from _powerboxes import distance_box_iou, parallel_distance_box_iou, remove_small_boxes, boxes_areas, box_convert
+
+__all__ = ["distance_box_iou", "parallel_distance_box_iou", "remove_small_boxes", "boxes_areas", "box_convert"]
---
+++ tests/test_speed.py
@@ -0,0 +1,3 @@
+import pytest
+from powerboxes import distance_box_iou, parallel_distance_box_iou, remove_small_boxes, boxes_areas, box_convert
+
