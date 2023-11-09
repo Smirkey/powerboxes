7e6ec863e04b1fe37e00c13b9a1110d4a6e69db1
Guillaume Bertrand <guillaume.bertrand59@gmail.com>
2023-11-09 22:17:34 +0100

    add type stub

--- python/powerboxes/__init__.py
+++ python/powerboxes/__init__.py
@@ -1,11 +1,10 @@
-import numpy as np
-from ._powerboxes import box_convert
-from ._powerboxes import boxes_areas
-from ._powerboxes import distance_box_iou
-from ._powerboxes import parallel_distance_box_iou
-from ._powerboxes import remove_small_boxes
-
-
+from ._powerboxes import (
+    box_convert,
+    boxes_areas,
+    distance_box_iou,
+    parallel_distance_box_iou,
+    remove_small_boxes,
+)
 
 __all__ = [
     "distance_box_iou",
