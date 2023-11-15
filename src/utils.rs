aee11b3c5081728981a976f318e9aa389754bab1
Guillaume Bertrand <guillaume.bertrand59@gmail.com>
2023-11-15 23:53:29 +0100

    tests & doc (#10)

--- src/utils.rs
+++ src/utils.rs
@@ -46,3 +46,41 @@ pub fn preprocess_array(
         .unwrap();
     return Ok(array);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ndarray::{ArrayBase, IxDyn};
+    use numpy::{PyArray, PyArrayDyn};
+
+    #[test]
+    fn test_array_to_numpy() {
+        let data = vec![1., 2., 3., 4.];
+        let array = ArrayBase::from_shape_vec((1, 4), data).unwrap();
+        Python::with_gil(|py| {
+            let result = array_to_numpy(py, array).unwrap();
+            assert_eq!(result.readonly().shape(), &[1, 4]);
+            assert_eq!(result.readonly().shape(), &[1, 4]);
+        });
+    }
+
+    #[test]
+    fn test_numpy_to_array() {
+        Python::with_gil(|python| {
+            let array = PyArray::<f64, _>::zeros(python, [2, 3], false);
+            let result = numpy_to_array(&array);
+            assert_eq!(result.shape(), &[2, 3]);
+        });
+    }
+
+    #[test]
+    fn test_preprocess_array() {
+        Python::with_gil(|python| {
+            let array = PyArrayDyn::<f64>::zeros(python, IxDyn(&[2, 4]), false);
+            let result = preprocess_array(&array);
+            assert!(result.is_ok());
+            let unwrapped_result = result.unwrap();
+            assert_eq!(unwrapped_result.shape(), &[2, 4]);
+        });
+    }
+}
